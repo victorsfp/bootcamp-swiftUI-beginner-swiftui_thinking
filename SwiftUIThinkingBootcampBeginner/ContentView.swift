fe713a797e785c080b46ddd2c6586a9496a72e34
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2025-11-28 20:52:24 -0300

    Project initialize

---
+++ SwiftUIThinkingBootcampBeginner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 28/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Placeholder")
+            .font(.title)
+            .fontWeight(.black)
+            .foregroundColor(Color.green)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIThinkingBootcampBeginner/SwiftUIThinkingBootcampBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampBeginnerApp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
