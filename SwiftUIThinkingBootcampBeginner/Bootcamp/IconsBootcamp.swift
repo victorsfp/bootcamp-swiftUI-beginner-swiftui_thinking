2f5ce78fc04f2dbd324ae52e5bf8eb21560c78b5
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2025-12-02 20:06:09 -0300

    System Icons, Multi-Color Icons, and sdk Symbols in SwiftUI

---
+++ SwiftUIThinkingBootcampBeginner/Bootcamp/IconsBootcamp.swift
@@ -0,0 +1,29 @@
+//
+//  IconsBootcamp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 02/12/25.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Image(systemName: "person.fill.badge.plus")
+            .renderingMode(.original)
+            .font(.largeTitle)
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .scaledToFit()
+//            .scaledToFill()
+//            .font(.caption)
+//            .font(.system(size: 200))
+//            .foregroundColor(.red)
+//            .frame(width: 300, height: 300)
+//            .clipped()
+    }
+}
+
+#Preview {
+    IconsBootcamp()
+}
