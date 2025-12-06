50ff1b3045d049aac7fdad5dfdb598079bf07ea0
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2025-12-06 13:14:00 -0300

    How to use Spacer in SwiftUI

---
+++ SwiftUIThinkingBootcampBeginner/Bootcamp/SpaceBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SpaceBootcamp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 06/12/25.
+//
+
+import SwiftUI
+
+struct SpaceBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpaceBootcamp()
+}
