64b85ae896ecf2b84e09dd8744c26f0e1acd9516
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2025-12-09 19:39:24 -0300

    LazyVGrid, LazyHGrid and GridItems in SwiftUI

---
+++ SwiftUIThinkingBootcampBeginner/Bootcamp/GridBootcamp.swift
@@ -0,0 +1,73 @@
+//
+//  GridBootcamp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 09/12/25.
+//
+
+import SwiftUI
+
+struct GridBootcamp: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil)
+    ]
+    
+    var body: some View {
+        ScrollView {
+            Rectangle()
+                .fill(Color.orange)
+                .frame(height: 400)
+            
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: 6,
+                pinnedViews: [.sectionHeaders]
+            ) {
+                Section(header:
+                            Text("Section 1")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.blue)
+                    .padding()
+                ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .frame(height: 150)
+                    }
+                }
+                
+                Section(header:
+                            Text("Section 2")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.red)
+                    .padding()
+                ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .fill(Color.green)
+                            .frame(height: 150)
+                    }
+                }
+                
+            }
+            
+//            LazyVGrid(columns: columns) {
+//                ForEach(0..<50) { index in
+//                    Rectangle()
+//                        .frame(height: 150)
+//                }
+//            }
+        }
+    }
+}
+
+#Preview {
+    GridBootcamp()
+}
