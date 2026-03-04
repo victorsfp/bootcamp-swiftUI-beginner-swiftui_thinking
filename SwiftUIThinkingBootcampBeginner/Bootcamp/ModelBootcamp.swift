bd616bc34314f78337015accb430d22d06b5f62a
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2026-03-03 21:00:17 -0300

    How to create custom models in SwiftUI

---
+++ SwiftUIThinkingBootcampBeginner/Bootcamp/ModelBootcamp.swift
@@ -0,0 +1,78 @@
+//
+//  ModelBootcamp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 03/03/26.
+//
+
+import SwiftUI
+
+struct UserModel: Identifiable {
+    let id: String = UUID().uuidString
+    let displayName: String
+    let userName: String
+    let followerCount: Int
+    let isVerified: Bool
+}
+
+struct ModelBootcamp: View {
+    
+    @State var users: [UserModel] = [
+//        "Nick", "Emily", "Samantha", "Chris"
+        UserModel(displayName: "Nick", userName: "nick123", followerCount: 100, isVerified: true),
+        UserModel(displayName: "Emily", userName: "itsemily1995", followerCount: 55, isVerified: false),
+        UserModel(displayName: "Samantha", userName: "ninja", followerCount: 355, isVerified: false),
+        UserModel(displayName: "Chris", userName: "chris2009", followerCount: 88, isVerified: true)
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(users) { user in
+                    HStack(spacing: 15.0) {
+                        Circle()
+                            .frame(width: 35, height: 35)
+                        
+                        VStack(alignment: .leading) {
+                            Text(user.displayName)
+                                .font(.headline)
+                            Text("@\(user.userName)")
+                                .foregroundStyle(.gray)
+                                .font(.caption)
+                        }
+                        
+                        Spacer()
+                        
+                        if user.isVerified {
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundStyle(.blue)
+                        }
+                        
+                        VStack {
+                            Text("\(user.followerCount)")
+                                .font(.headline)
+                            Text("Followers")
+                                .foregroundStyle(.gray)
+                                .font(.caption)
+                        }
+                    }
+                    .padding(.vertical, 10)
+                }
+//                ForEach(users, id: \.self) { name in
+//                    HStack(spacing: 15.0) {
+//                        Circle()
+//                            .frame(width: 35, height: 35)
+//                        Text(name)
+//                    }
+//                    .padding(.vertical, 10)
+//                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Users")
+        }
+    }
+}
+
+#Preview {
+    ModelBootcamp()
+}
