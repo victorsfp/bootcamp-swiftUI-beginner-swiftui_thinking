23ab6389ed82d8495dd13cbcd6fef79884ec582b
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2026-03-22 13:25:41 -0300

    Async Image in iOS 15 for SwiftUI

---
+++ SwiftUIThinkingBootcampBeginner/Bootcamp/AsyncImageBootcamp.swift
@@ -0,0 +1,53 @@
+//
+//  AsyncImageBootcamp.swift
+//  SwiftUIThinkingBootcampBeginner
+//
+//  Created by Victor dos Santos Feitosa on 22/03/26.
+//
+
+import SwiftUI
+
+/*
+ case empty -> No image is loaded
+ case success(Image) -> An image succesfully loaded.
+ case failure(Error) -> An image failed to load with an error.
+ */
+struct AsyncImageBootcamp: View {
+    
+    let url = URL(string: "https://picsum.photos/400")
+    
+    var body: some View {
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let returnedImage):
+                returnedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
+            case .failure(let error):
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            }
+        }
+        
+//        AsyncImage(url: url, content: { returnedImage in
+//            returnedImage
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 100, height: 100)
+//                .cornerRadius(20)
+//        }, placeholder: {
+//            ProgressView()
+//        })
+    }
+}
+
+#Preview {
+    AsyncImageBootcamp()
+}
